402e5550b49f1b65a72dd56fbf8a2cb45662c413
Matheus Tasso <matheustassodjamdjian@gmail.com>
2026-05-30 17:26:00 -0300

    feat: Ajustes na lista de detritos e lista satélites operacionais

---
+++ orbita_segura.py
@@ -0,0 +1,63 @@
+"""
+ORBITA SEGURA - Sistema de Monitoramento e Prevencao de Colisoes com Detritos Espaciais
+Global Solution - FIAP - 1o Semestre
+Integrantes:
+    Matheus Tasso Djamdjian   - RM 57076
+    Daniel Silva Boccia        - RM 569617
+    Matheus Augusto da Silva   - RM 572976
+    Kaik Sales de Amorim       - RM 571558
+"""
+
+# ============================================================================
+# LISTA 1 - DETRITOS ESPACIAIS RASTREADOS (20 itens)
+# Estrutura: [ID NORAD, Nome do Objeto, Origem, Altitude (km), Tamanho (cm)]
+# ============================================================================
+detritos = [
+    [25730, "Fengyun-1C Frag A",   "China ASAT 2007",      865,  12],
+    [33759, "Cosmos 2251 Frag",    "Colisao Iridium 2009", 790,  18],
+    [34427, "Iridium 33 Frag",     "Colisao 2009",         780,   9],
+    [11871, "Delta 1 R/B",         "EUA Estagio 1975",     950,  85],
+    [16615, "Ariane 1 Debris",     "ESA Estagio 1986",    1450,  45],
+    [22675, "Cosmos 1408 Frag",    "Russia ASAT 2021",     480,  15],
+    [27006, "Pegasus HAPS",        "EUA Estagio 2001",     645,  30],
+    [38746, "CZ-4B R/B Frag",      "China Estagio 2012",   780,  22],
+    [43286, "Falcon 9 Upper",      "SpaceX 2018",          540,  60],
+    [40115, "Briz-M Tank",         "Russia Estagio 2014",  690,  75],
+    [25400, "SL-16 R/B",           "Russia Estagio 1992", 1010,  90],
+    [16193, "Titan Transtage",     "EUA Estagio 1965",   35780, 120],
+    [37728, "ENVISAT Frag",        "ESA Falha 2012",       790,  25],
+    [27424, "Aqua Debris",         "EUA Erosao 2002",      705,   8],
+    [29275, "GOES-13 Frag",        "EUA Impacto MMOD",   35790,  14],
+    [42758, "Long March 7 Frag",   "China 2017",           450,  35],
+    [44432, "Vega Upper Stage",    "ESA Estagio 2019",     720,  55],
+    [45623, "Starlink Debris",     "SpaceX Falha 2020",    550,  10],
+    [46289, "OneWeb Frag",         "UK Falha 2021",       1200,  16],
+    [48274, "CZ-5B R/B",           "China Reentrada 2021", 380,  95]
+]
+
+# ============================================================================
+# LISTA 2 - SATELITES OPERACIONAIS MONITORADOS (20 itens)
+# Estrutura: [Nome, Operadora, Funcao, Orbita, Valor Estimado (US$ milhoes)]
+# ============================================================================
+satelites = [
+    ["Starlink-3421",    "SpaceX",   "Comunicacao",   "LEO",   1.5],
+    ["Amazonia-1",       "INPE",     "Observacao",    "LEO",  85.0],
+    ["CBERS-4A",         "INPE",     "Observacao",    "LEO", 100.0],
+    ["GPS IIF-12",       "USAF",     "Navegacao",     "MEO", 245.0],
+    ["Galileo FOC-22",   "ESA",      "Navegacao",     "MEO", 220.0],
+    ["GOES-16",          "NOAA",     "Meteorologico", "GEO", 500.0],
+    ["Hubble Space Tel", "NASA",     "Cientifico",    "LEO",2500.0],
+    ["ISS",              "NASA/ROS", "Tripulada",     "LEO",150000.0],
+    ["Sentinel-2A",      "ESA",      "Observacao",    "LEO", 165.0],
+    ["Landsat-9",        "NASA",     "Observacao",    "LEO", 750.0],
+    ["SGDC-1",           "Visiona",  "Comunicacao",   "GEO", 600.0],
+    ["Intelsat-37e",     "Intelsat", "Comunicacao",   "GEO", 400.0],
+    ["Iridium NEXT-148", "Iridium",  "Comunicacao",   "LEO",  80.0],
+    ["OneWeb-0224",      "OneWeb",   "Comunicacao",   "LEO",   1.2],
+    ["Terra (EOS AM-1)", "NASA",     "Cientifico",    "LEO", 800.0],
+    ["JWST",             "NASA",     "Cientifico",    "L2",10000.0],
+    ["Tiangong CSS",     "CNSA",     "Tripulada",     "LEO",8000.0],
+    ["Beidou-3 M24",     "CNSA",     "Navegacao",     "MEO", 200.0],
+    ["GLONASS-K2",       "Roscosmos","Navegacao",     "MEO", 180.0],
+    ["Amazonas Nexus",   "Hispasat", "Comunicacao",   "GEO", 320.0]
+]
